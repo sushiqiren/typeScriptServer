049e1a6fcf0b08eaf3c1fc5fa264d625b1b080c1
sushiqiren <hzfulideman@gmail.com>
2025-06-12 16:09:59 +1000

    change api end point route

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -2,6 +2,15 @@ import type { Request, Response } from "express";
 import { config } from "../config.js";
 
 export async function handlerMetrics(_: Request, res: Response) {
-  res.send(`Hits: ${config.fileserverHits}`);
+  const htmlContent = `
+    <html>
+      <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+      </body>
+    </html>
+    `;
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.send(htmlContent);
   res.end();
 }
--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ const PORT = 8080;
 app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerMetrics);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
