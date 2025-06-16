a9a1eb99fba975b6c94b705f94ce1e92303dc773
sushiqiren <hzfulideman@gmail.com>
2025-06-16 16:51:35 +1000

    add database url to connection and set up data insertion

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,23 @@
+process.loadEnvFile();
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (value === undefined) {
+    throw new Error(`Required environment variable "${key}" is missing`);
+  }
+  return value;
+}
+
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 }
 
 // create a config object that will hold the stateful data and this object can be imported in other files
 
 const config: APIConfig = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
 
 export { config };
